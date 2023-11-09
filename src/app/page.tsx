bff751c64046484be021e99e9b904213bf14236e
taib.abroug <taibabro@gmail.com>
2023-11-09 16:33:17 +0100

    fix favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Head from 'next/head';
 import Image from 'next/image';
 import {useEffect, useState} from "react";
 const targetTime = new Date("2024-04-14").getTime();
@@ -22,9 +21,7 @@ export default function Home() {
 
     return (
         <div className='flex flex-col items-center lg:flex-row lg:justify-center lg:gap-3  '>
-               <Head>
-               <link rel="icon" type="image/png" href="./favicon.png" />
-    </Head>
+            
            <div className=' text-black text-center content max-w-[550px] h-fit rounded-[12px] p-10'>
       
             <div>
